4f8eb19ae48fddefd480ea013d3786eb1a5fba73
papparazzo <pappi-@gmx.de>
2023-10-24 21:09:56 +0200

    alter table added

--- sql/newSQLTemplate.sql
+++ sql/newSQLTemplate.sql
@@ -10,5 +10,7 @@ CREATE TABLE IF NOT EXISTS `{TABLE_PREFIX}_imagegalleries` (
     `Name` char(50) COLLATE utf8_unicode_ci NOT NULL,
     `Description` text COLLATE utf8_unicode_ci NOT NULL,
     `PreviewImage` varchar(25) COLLATE utf8_unicode_ci NOT NULL
-    PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `{TABLE_PREFIX}_imagegalleries` ADD PRIMARY KEY (`Id`);
+
